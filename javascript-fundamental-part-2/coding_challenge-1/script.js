682d778b6a6ec14ee1952851424237ccb596d7d8
progmamun <mamun333089@gmail.com>
2021-12-06 18:34:59 +0600

    coding-challenge--1

---
+++ javascript-fundamental-part-2/coding_challenge-1/script.js
@@ -0,0 +1,25 @@
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+// test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgKoalas} vs. ${avgDolphins})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(576, 111);
+
+// Test 2
+scoreDolphins = calcAverage(44, 23, 71);
+scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
