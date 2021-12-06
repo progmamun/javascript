274c5791e79b64ea3ac0ed5583466cbc4b5060c9
progmamun <mamun333089@gmail.com>
2021-12-06 15:17:20 +0600

    functions

---
+++ javascript-fundamental-part-2/function/script.js
@@ -0,0 +1,19 @@
+function logger() {
+  console.log('My name is Mamun');
+}
+// calling / running/ invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
