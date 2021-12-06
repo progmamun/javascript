4992f08ad35da58642f0462558053df02e0210b3
progmamun <mamun333089@gmail.com>
2021-12-06 16:52:10 +0600

    arrow functions

--- javascript-fundamental-part-2/script.js
+++ javascript-fundamental-part-2/script.js
@@ -13,4 +13,19 @@ const calcAge2 = function (birthYear) {
 }
 const age2 = calcAge2(1995);
 
-console.log(age1, age2);
+console.log(age1, age2)
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1997);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1999,'Mamun'));
+console.log(yearsUntilRetirement(1995, 'Mimi'));
