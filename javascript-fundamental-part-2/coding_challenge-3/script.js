fce93b891171d851083df5b560c9b41b696322de
progmamun <mamun333089@gmail.com>
2021-12-07 10:30:59 +0600

    coding_challenge-2

---
+++ javascript-fundamental-part-2/coding_challenge-3/script.js
@@ -0,0 +1,29 @@
+const mark = {
+  fullName: 'Al Mamun Khan',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function() {
+    this.bmi = this.mass / this.height ** 2;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi})is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI (${john.bmi})is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
