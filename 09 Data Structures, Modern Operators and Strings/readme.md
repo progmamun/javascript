5c0c6375416fa728717f42cc2f7c6f51f8364283
progmamun <mamun333089@gmail.com>
2021-12-10 16:48:40 +0600

    spread-operator

--- 09 Data Structures, Modern Operators and Strings/readme.md
+++ 09 Data Structures, Modern Operators and Strings/readme.md
@@ -1,1 +1,3 @@
 # Data Structures, Modern Operators and Strings
+
+## Iterables: arrays, strings, maps, sets, NOT objects
---
+++ 09 Data Structures, Modern Operators and Strings/spread_operator/script.js
@@ -0,0 +1,18 @@
+'use strict';
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [];
