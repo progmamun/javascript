9dbaa7fb68dcf254041e9254eba77eec5cd0b87e
progmamun <mamun333089@gmail.com>
2021-12-12 20:16:55 +0600

    strings

---
+++ 09 Data Structures, Modern Operators and Strings/strings/readme.md
@@ -0,0 +1,3 @@
+## String-Javascript
+
+Strings(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
--- 09 Data Structures, Modern Operators and Strings/strings/script.js
+++ 09 Data Structures, Modern Operators and Strings/strings/script.js
@@ -51,3 +51,49 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
+
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Al Mamun Khan'.split(' '));
+
+const [firstName, lastName] = 'Al Mamun Khan'.split(' ');
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('Jessica ann smith davis');
+capitalizeName('Al Mamun Khan');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(23, '+').padEnd(35, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(492982892719177));
+console.log(maskCreditCard('9229791619691616396'));
+
+// Repeat
+const message2 = 'Bad weather... All Departues Delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'5'.repeat(n)}`);
+  planesInLine(5);
+  planesInLine(3);
+  planesInLine(12);
+};
