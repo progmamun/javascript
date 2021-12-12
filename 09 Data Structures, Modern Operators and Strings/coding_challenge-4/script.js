2cf1e39ff73f59897c09afa208d8c429b4ef2084
progmamun <mamun333089@gmail.com>
2021-12-12 21:07:25 +0600

    coding_challenge-4

---
+++ 09 Data Structures, Modern Operators and Strings/coding_challenge-4/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows);
+  for (const row of rows) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
