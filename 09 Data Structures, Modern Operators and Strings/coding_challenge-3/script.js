88ca05c313abf2ea6688bdf6eae32a69f88fdf9c
progmamun <mamun333089@gmail.com>
2021-12-12 11:04:07 +0600

    data_structure

---
+++ 09 Data Structures, Modern Operators and Strings/coding_challenge-3/script.js
@@ -0,0 +1,36 @@
+const gameEvents = new Map([
+  [17, 'Goal'],
+  [36, 'substitution'],
+  [47, 'Goal'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'Goal'],
+  [80, 'Goal'],
+  [92, 'Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}}`);
+}
