6a8ab08e852c4d52b9ec0b147f14ce3dc676e244
progmamun <mamun333089@gmail.com>
2021-12-12 08:48:34 +0600

    Set unique value(remove dubble vale)

---
+++ 09 Data Structures, Modern Operators and Strings/sets/script.js
@@ -0,0 +1,18 @@
+'use strict';
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+console.log(ordersSet);
