4e86e21b4193ab7e9b60527e4d75f548791f26ef
progmamun <mamun333089@gmail.com>
2021-12-13 08:42:29 +0600

    passing-arguments vs reference

--- 10 A Closer Look at Functions/default_parameters/script.js
+++ 10 A Closer Look at Functions/default_parameters/script.js
@@ -26,3 +26,36 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+
+// How passing arguments works_value vs Reference
+const flight = 'LH234';
+const mamun = {
+  name: 'Al Mamun Khan',
+  passport: 23939499,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23939499) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, mamun);
+// console.log(flight);
+// console.log(mamun);
+
+// Is the same as doing..
+// const flightNum = flight;
+// const passenger = mamun;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000);
+};
+
+newPassport(mamun);
+checkIn(flight, mamun);
