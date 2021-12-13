d507f21474c607853673542999148d0e585623d5
progmamun <mamun333089@gmail.com>
2021-12-13 10:58:58 +0600

    callback function

---
+++ 10 A Closer Look at Functions/callback_functions/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Mamun', 'Khan', 'Adem'].forEach(high5);
