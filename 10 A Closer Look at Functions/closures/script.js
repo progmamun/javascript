a0e8e42b429293f080b6217947f3ddaceddc452c
progmamun <mamun333089@gmail.com>
2021-12-14 19:33:18 +0600

    closures

--- 10 A Closer Look at Functions/closures/script.js
+++ 10 A Closer Look at Functions/closures/script.js
@@ -16,3 +16,45 @@ booker();
 booker();
 
 console.dir(dir);
+
+// Part 2
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
