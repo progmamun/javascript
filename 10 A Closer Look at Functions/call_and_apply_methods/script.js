5da4466124741f86f784c45a585615aaa4c97765
progmamun <mamun333089@gmail.com>
2021-12-14 09:38:04 +0600

    call-and-apply-methods

---
+++ 10 A Closer Look at Functions/call_and_apply_methods/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Al Mamun Khan');
+lufthansa.book(635, 'Mamun Khan');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+// Does not work (bcz this key used)
+book(23, 'Mamun Khan');
+
+// Call method
+book.call(eurowings, 23, 'Mamun Khan');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
