9c5c5ae4e473c5c3cd4f2af87436a825d76f0ffa
progmamun <mamun333089@gmail.com>
2022-01-04 22:37:22 +0600

    update

--- 14 Object-Oriented Programming (OOP) With JavaScript/starter/script.js
+++ 14 Object-Oriented Programming (OOP) With JavaScript/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  console.log(this);
+};
+new Person('Mamun', 1999);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+//
+// 4. function automatically return {}
