9ab7a7c03b744ecfc345f4992567ecb032562f41
progmamun <mamun333089@gmail.com>
2022-01-27 17:03:56 +0600

    type-conversion

---
+++ javascript-fundamental-part-1/type-conversion.js
@@ -0,0 +1,8 @@
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
