463ad5c90f5e4a8bb119a75a2c863c9677291c83
progmamun <mamun333089@gmail.com>
2022-01-28 10:27:01 +0600

    arrow-function

--- javascript-fundamental-part-2/arrow-function.js
+++ javascript-fundamental-part-2/arrow-function.js
@@ -4,3 +4,19 @@
 const calcAge3 = birthYear => 2022 - birthYear;
 const age3 = calcAge3(1999);
 console.log(age3);
+
+/* const yearsUntilRetirement = birthYear => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+};
+
+console.log(yearsUntilRetirement(1991)); */
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+console.log(yearsUntilRetirement(1999, 'Mamun'));
+console.log(yearsUntilRetirement(1980, 'Al Mamun'));
