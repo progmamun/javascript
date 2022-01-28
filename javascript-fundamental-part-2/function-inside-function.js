3244a771b14a9a408282cba6c297f3daf5d62e3f
progmamun <mamun333089@gmail.com>
2022-01-28 11:20:44 +0600

    function-inside-function

---
+++ javascript-fundamental-part-2/function-inside-function.js
@@ -0,0 +1,14 @@
+'use strict';
+const cutPieces = function (fruit) {
+  return fruit * 4;
+};
+
+const fruitProcessor = function (apples, oranges) {
+  const applePieces = cutPieces(apples);
+  const orangePieces = cutPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+};
+
+console.log(fruitProcessor(2, 3));
