4d9b768e3b6c1ce2124963c561585b61bcaafcdc
progmamun <mamun333089@gmail.com>
2022-02-03 10:06:17 +0600

    nav-hover-animation

--- 13 Advanced DOM and Event/script.js
+++ 13 Advanced DOM and Event/script.js
@@ -116,24 +116,25 @@ tabsContainer.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
 });
 //============= Menu fade animation =================//
-const handleHover = function (e, opacity) {
+const handleHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if (el !== link) el.style.opacity = opacity;
+      if (el !== link) el.style.opacity = this;
     });
+    logo.style.opacity = this;
   }
 };
-nav.addEventListener('mouseover', function (e) {
-  handleHover(e, 0.5);
-});
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
 
-nav.addEventListener('mouseout', function (e) {
-  handleHover(e, 1);
-});
+nav.addEventListener('mouseout', handleHover.bind(1));
 //============= EventListener =================//
 /*
 const h1 = document.querySelector('h1');
