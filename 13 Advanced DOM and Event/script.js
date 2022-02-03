514187ad565851f3f3adcfa7b16d07ff4de69281
progmamun <mamun333089@gmail.com>
2022-02-03 17:28:19 +0600

    slider

--- 13 Advanced DOM and Event/script.js
+++ 13 Advanced DOM and Event/script.js
@@ -193,7 +193,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  // section.classList.add('section--hidden');
+  section.classList.add('section--hidden');
 });
 
 //============= Lazy loading Images =================//
