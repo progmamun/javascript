a7963c86607d7943dab95c1daabe61618922e51b
progmamun <mamun333089@gmail.com>
2022-02-03 16:23:22 +0600

    slider

--- 13 Advanced DOM and Event/script.js
+++ 13 Advanced DOM and Event/script.js
@@ -221,6 +221,48 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+//============= Slider Section =================//
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.4) translateX(-800px)';
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`))
+  );
+};
+goToSlide(0);
+
+// 0% , 100%, 200%, 300%
+
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
 //============= EventListener =================//
 /*
 const h1 = document.querySelector('h1');
