35fc46abd958f7413d7a65c197e054ee519a6534
progmamun <mamun333089@gmail.com>
2022-02-24 17:38:33 +0600

    promisifying-setTimeout

--- 16 Asynchronous JavaScript_ Promises, Async_Await, and AJAX/script.js
+++ 16 Asynchronous JavaScript_ Promises, Async_Await, and AJAX/script.js
@@ -224,7 +224,8 @@ const whereAmI = function (lat, lng) {
 
 whereAmI(52.508, 13.381);
 */
-
+////--=======---////
+/*
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved promise 1').then(res => console.log(res));
 
@@ -234,3 +235,34 @@ Promise.resolve('Resolved promise 2').then(res => {
   }
 });
 console.log('Test end');
+*/
+/////---=======-----////////
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You Win');
+    } else {
+      reject('You lost your money');
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// --=== Promisifying setTimeout ---==== ////
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.log(x));
